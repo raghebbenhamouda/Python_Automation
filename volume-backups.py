e109702222ddfdb29e4aa4ca4a12a7b267e1f866
raghebbenhamouda <ragheb.benhamouda@ensi-uma.tn>
2022-06-15 17:37:22 +0100

    add volume-backup file

---
+++ volume-backups.py
@@ -0,0 +1,18 @@
+import boto3
+import schedule
+ec2_client = boto3.client('ec2',region_name="eu-west-3")
+
+def create_volume_snapshots():
+    volumes = ec2_client.describe_volumes()
+
+    for volume in volumes['Volumes']:
+        new_snapshot = ec2_client.create_snapshot(
+        VolumeId=volume['VolumeId'],
+    )
+        print(new_snapshot)
+
+schedule.every(5).days.do(create_volume_snapshots)
+
+while True: 
+    schedule.run_pending()
+
